58e5dbcb97100b6bdea358c368dea27fdf62f21d
DevDaniëls <daniel.geerts@live.nl>
2022-01-06 14:42:02 +0100

    Disable tf k8

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,60 +1,60 @@
-# Configure Kubernetes provder
-provider "kubernetes" {}
+# # Configure Kubernetes provder
+# provider "kubernetes" {}
 
-# Create Kubernetes namespace
-resource "kubernetes_namespace" "cicd" {
-  metadata {
-    name = var.namespace
-  }
-}
+# # Create Kubernetes namespace
+# resource "kubernetes_namespace" "cicd" {
+#   metadata {
+#     name = var.namespace
+#   }
+# }
 
-# Create Kubernetes DEPLOYMENT
-resource "kubernetes_deployment" "cicd" {
-  metadata {
-    name      = "nodeapp-deployment"
-    namespace = kubernetes_namespace.cicd.metadata.0.name
-  }
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = "MyNodeApp"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "MyNodeApp"
-        }
-      }
-      spec {
-        container {
-          image = "icr.io/ibm-daniels-workspace/cicd-casestudy-app:latest" #path = "/app"
-          name  = "nodeapp-container"
-          port {
-            container_port = 3000
-          }
-        }
-      }
-    }
-  }
-}
+# # Create Kubernetes DEPLOYMENT
+# resource "kubernetes_deployment" "cicd" {
+#   metadata {
+#     name      = "nodeapp-deployment"
+#     namespace = kubernetes_namespace.cicd.metadata.0.name
+#   }
+#   spec {
+#     replicas = 1
+#     selector {
+#       match_labels = {
+#         app = "MyNodeApp"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           app = "MyNodeApp"
+#         }
+#       }
+#       spec {
+#         container {
+#           image = "icr.io/ibm-daniels-workspace/cicd-casestudy-app:latest" #path = "/app"
+#           name  = "nodeapp-container"
+#           port {
+#             container_port = 3000
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
-# Create Kubernetes SERVICE
-resource "kubernetes_service" "cicd" {
-  metadata {
-    name = "nodeapp-service"
-  }
-  spec {
-    selector = {
-      app = kubernetes_deployment.cicd.spec.0.template.0.metadata.0.labels.app
-    }
-    port {
-      node_port   = 32100
-      port        = 3000
-      target_port = 3000
-      protocol    = "TCP"
-    }
-    type = "NodePort"
-  }
-}
+# # Create Kubernetes SERVICE
+# resource "kubernetes_service" "cicd" {
+#   metadata {
+#     name = "nodeapp-service"
+#   }
+#   spec {
+#     selector = {
+#       app = kubernetes_deployment.cicd.spec.0.template.0.metadata.0.labels.app
+#     }
+#     port {
+#       node_port   = 32100
+#       port        = 3000
+#       target_port = 3000
+#       protocol    = "TCP"
+#     }
+#     type = "NodePort"
+#   }
+# }
